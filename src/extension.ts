8272a7b9884bca58cc15c8589032b06dcb5bbd60
akeeton <andrewrkeeton@gmail.com>
2024-11-09 15:10:14 -0500

    Add start/stop commands and refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,34 @@
 import * as vscode from 'vscode';
+import { Duration, Interval } from 'luxon';
 
-export function activate(context: vscode.ExtensionContext) {
-	console.log(`Extension activated: ${context.extensionUri}`);
+export function activate({ extension, subscriptions }: vscode.ExtensionContext) {
+	function makeCommmandId(commandName: string) {
+		return `${extension.packageJSON.name}.${commandName}`;
+	}
 
-	const clickTimerCommandId = 'statusbartimer.clickTimer';
-	const clickTimerCommand = vscode.commands.registerCommand(clickTimerCommandId, () => {
-		vscode.window.showInformationMessage('Clicked on status bar timer');
-	});
-	context.subscriptions.push(clickTimerCommand);
+	console.log(`Extension activated: ${extension.id}`);
+
+	subscriptions.push(
+		vscode.commands.registerCommand(
+			makeCommmandId('startTimer'), () => {
+				vscode.window.showInformationMessage('Started timer');
+			}));
+
+	subscriptions.push(
+		vscode.commands.registerCommand(
+			makeCommmandId('stopTimer'), () => {
+				vscode.window.showInformationMessage('Stopped timer');
+			}));
+
+	const clickStatusBarItemId = makeCommmandId('clickStatusBarItem');
+	subscriptions.push(
+		vscode.commands.registerCommand(clickStatusBarItemId, () => {
+			vscode.window.showInformationMessage('Clicked on status bar timer');
+		}));
 
 	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-	statusBarItem.command = clickTimerCommandId;
-	context.subscriptions.push(statusBarItem);
+	statusBarItem.command = clickStatusBarItemId;
+	subscriptions.push(statusBarItem);
 
 	const updateStatusBarItem = () => {
 		console.log('Updating status bar item');
