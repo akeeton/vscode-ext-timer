7f9335b6077ad8c22df84bf9fd79731999fb5ddc
akeeton <andrewrkeeton@gmail.com>
2024-11-09 16:50:54 -0500

    Track start/stop intervals calculate duration

--- src/extension.ts
+++ src/extension.ts
@@ -1,46 +1,74 @@
 import * as vscode from 'vscode';
-import { Duration, Interval } from 'luxon';
+import { commands, window } from 'vscode';
+
+import { DateTime, Duration, Interval } from 'luxon';
+
+const registerCommand = commands.registerCommand;
+const showInformationMessage = window.showInformationMessage;
 
 export function activate({ extension, subscriptions }: vscode.ExtensionContext) {
-	function makeCommmandId(commandName: string) {
+	function makeCommandId(commandName: string) {
 		return `${extension.packageJSON.name}.${commandName}`;
 	}
 
 	console.log(`Extension activated: ${extension.id}`);
 
-	subscriptions.push(
-		vscode.commands.registerCommand(
-			makeCommmandId('startTimer'), () => {
-				vscode.window.showInformationMessage('Started timer');
-			}));
+	let lastStartTime: DateTime | null = null;
+	const intervals: Interval[] = [];
+	const statusBarItemUpdateInMs = 1000;
 
-	subscriptions.push(
-		vscode.commands.registerCommand(
-			makeCommmandId('stopTimer'), () => {
-				vscode.window.showInformationMessage('Stopped timer');
-			}));
+	const clickStatusBarItemId = makeCommandId('clickStatusBarItem');
+	subscriptions.push(registerCommand(clickStatusBarItemId, () => {
+		showInformationMessage('Clicked on status bar timer');
+	}));
 
-	const clickStatusBarItemId = makeCommmandId('clickStatusBarItem');
-	subscriptions.push(
-		vscode.commands.registerCommand(clickStatusBarItemId, () => {
-			vscode.window.showInformationMessage('Clicked on status bar timer');
-		}));
-
-	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	const statusBarItem = window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 	statusBarItem.command = clickStatusBarItemId;
 	subscriptions.push(statusBarItem);
 
 	const updateStatusBarItem = () => {
-		console.log('Updating status bar item');
-		statusBarItem.text = '$(clock) timer';
+		let intervalsStoppedNow: Interval[];
+		if (!lastStartTime) {
+			intervalsStoppedNow = intervals;
+		} else {
+			intervalsStoppedNow = intervals.concat(
+				Interval.fromDateTimes(lastStartTime, DateTime.utc())
+			);
+		}
+
+		const duration = intervalsStoppedNow.map((interval) => {
+			return interval.toDuration();
+		}).reduce((durationAcc, duration) => {
+			return durationAcc.plus(duration);
+		}, Duration.fromObject({}));
+
+		statusBarItem.text = `$(clock) ${duration.toFormat('hh:mm:ss')}`;
 	};
 
 	updateStatusBarItem();
 	statusBarItem.show();
+	setInterval(updateStatusBarItem, statusBarItemUpdateInMs);
 
-	setTimeout(() => {
-		
-	}, 1000);
+	subscriptions.push(registerCommand(makeCommandId('startTimer'), () => {
+		if (!lastStartTime) {
+			lastStartTime = DateTime.utc();
+			showInformationMessage('Started timer');
+		} else {
+			showInformationMessage('Timer already started');
+		}
+	}));
+
+	subscriptions.push(registerCommand(makeCommandId('stopTimer'), () => {
+		if (!lastStartTime) {
+			showInformationMessage('Timer already stopped');
+		} else {
+			intervals.push(
+				Interval.fromDateTimes(lastStartTime, DateTime.utc())
+			);
+			lastStartTime = null;
+			showInformationMessage('Stopped timer');
+		}
+	}));
 }
 
 export function deactivate() {}
