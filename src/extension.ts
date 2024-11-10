0b8eaf918a739f79a569f052fc159933ad0d1a95
akeeton <andrewrkeeton@gmail.com>
2024-11-10 14:20:17 -0500

    Persist StartStopTimes and add resetTimer

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,66 @@ import * as vscode from 'vscode';
 import { commands, window } from 'vscode';
 
 import { DateTime, Duration, Interval } from 'luxon';
+import { start } from 'repl';
 
 const registerCommand = commands.registerCommand;
 const showInformationMessage = window.showInformationMessage;
+
+type StartStopTimesDto = {
+	lastStartTime?: string;
+	intervals: string[];
+}
+
+// TODO: Somehow finish open interval and save to storage on shutdown
+// TODO: Move to its own file
+class StartStopTimes {
+	lastStartTime?: DateTime;
+	intervals: Interval[] = [];
+
+	private static storageKey = 'startStopTimes';
+
+	reset = (): void => {
+		this.lastStartTime = undefined;
+		this.intervals = [];
+	};
+
+	saveToStorage = (memento: vscode.Memento): void => {
+		memento.update(StartStopTimes.storageKey, this.toDto());
+	};
+
+	/**
+	 * Loads stored data in-place from the given memento.
+	 * @param memento
+	 */
+	loadFromStorage = (memento: vscode.Memento): void => {
+		this.fromDto(memento.get(StartStopTimes.storageKey));
+	};
+
+	// TODO: Necessary? Currently using reset() then saveToStorage().
+	// clearStorage = (memento: vscode.Memento): void => {
+	// 	memento.update(StartStopTimes.storageKey, undefined);
+	// };
+
+	private toDto = (): StartStopTimesDto => {
+		return {
+			lastStartTime: this.lastStartTime?.toISO() ?? undefined,
+			intervals: this.intervals.map((i) => i.toISO()),
+		};
+	};
+
+	private fromDto = (dto?: StartStopTimesDto): void => {
+		if (!dto) {
+			this.reset();
+			return;
+		}
+
+		this.lastStartTime = dto.lastStartTime
+			? DateTime.fromISO(dto.lastStartTime)
+			: undefined;
+
+		this.intervals = dto.intervals.map((s) => Interval.fromISO(s));
+	};
+}
 
 export function activate({
 	extension,
@@ -17,15 +74,7 @@ export function activate({
 
 	console.log(`Extension activated: ${extension.id}`);
 
-	type StartStopTimes = {
-		lastStartTime: DateTime | null,
-		intervals: Interval[],
-	}
-
-	const startStopTimes: StartStopTimes = workspaceState.get('startStopTimes', {
-		lastStartTime: null,
-		intervals: [],
-	});
+	const startStopTimesKey = 'startStopTimes';
 
 	// TODO: Move to user settings
 	const statusBarItemUpdateInMs = 1000;
@@ -38,6 +87,9 @@ export function activate({
 	const statusBarItem = window.createStatusBarItem(vscode.StatusBarAlignment.Right);
 	statusBarItem.command = clickStatusBarItemId;
 	subscriptions.push(statusBarItem);
+
+	const startStopTimes = new StartStopTimes();
+	startStopTimes.loadFromStorage(workspaceState);
 
 	const updateStatusBarItem = () => {
 		let intervalsStoppedNow: Interval[];
@@ -63,6 +115,12 @@ export function activate({
 	statusBarItem.show();
 	setInterval(updateStatusBarItem, statusBarItemUpdateInMs);
 
+	subscriptions.push(registerCommand(makeCommandId('resetTimer'), () => {
+		startStopTimes.reset();
+		startStopTimes.saveToStorage(workspaceState);
+		updateStatusBarItem();
+	}));
+
 	subscriptions.push(registerCommand(makeCommandId('startTimer'), () => {
 		if (!startStopTimes.lastStartTime) {
 			startStopTimes.lastStartTime = DateTime.utc();
@@ -70,6 +128,8 @@ export function activate({
 		} else {
 			showInformationMessage('Timer already started');
 		}
+
+		startStopTimes.saveToStorage(workspaceState);
 	}));
 
 	subscriptions.push(registerCommand(makeCommandId('stopTimer'), () => {
@@ -79,12 +139,16 @@ export function activate({
 			startStopTimes.intervals.push(
 				Interval.fromDateTimes(startStopTimes.lastStartTime, DateTime.utc())
 			);
-			startStopTimes.lastStartTime = null;
+			startStopTimes.lastStartTime = undefined;
 			showInformationMessage('Stopped timer');
 		}
+
+		startStopTimes.saveToStorage(workspaceState);
 	}));
 }
 
 export function deactivate() {
-	// TODO: Save lastStartTime to intervals and set to null
+	// deactivate() seems to only be meant for cleaning up OS resources, not a
+	// general-purpose shutdown callback.  See
+	// https://github.com/Microsoft/vscode/issues/47881.
 }
