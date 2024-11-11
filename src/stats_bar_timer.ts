f61c1e5eaa3852cc725a0a3e72382f9bb7b99be1
akeeton <andrewrkeeton@gmail.com>
2024-11-11 14:46:37 -0500

    Replace StatusBarCommands interface with keyof typeof this.commands

--- src/stats_bar_timer.ts
+++ src/stats_bar_timer.ts
@@ -2,17 +2,8 @@ import { DateTime, Duration, Interval } from 'luxon';
 import * as vscode from 'vscode';
 import { StartStopTimes } from './start_stop_times';
 
-interface StatusBarCommands {
-	startTimer(): void;
-	stopTimer(): void;
-	resetTimer(): void;
-	clickStatusBarItem(): void;
-	debugShowWorkspaceStorage(): void;
-	debugClearAllWorkspaceStorage(): void;
-}
-
 // TODO: Replace checking this.startStopTimes.lastStartTime with an isRunning() function
-export class StatusBarTimer implements StatusBarCommands {
+export class StatusBarTimer {
 	constructor(
 		private context: vscode.ExtensionContext,
 		private startStopTimes: StartStopTimes,
@@ -38,33 +29,12 @@ export class StatusBarTimer implements StatusBarCommands {
 		console.log(`Extension ${this.context.extension.id} activated`);
 	};
 
-	private getDisplayName() {
+	private getDisplayName = () => {
 		return this.context.extension.packageJSON.displayName;
-	}
-
-	private makeCommandId = (commandName: keyof StatusBarCommands) => {
-		return `${this.context.extension.packageJSON.name}.${commandName}`;
 	};
 
-	private registerCommand(commandName: keyof StatusBarCommands) {
-		this.context.subscriptions.push(vscode.commands.registerCommand(
-			this.makeCommandId(commandName), this[commandName]
-		));
-	}
-
-	private registerCommands() {
-		this.registerCommand('startTimer');
-		this.registerCommand('stopTimer');
-		this.registerCommand('resetTimer');
-		this.registerCommand('clickStatusBarItem');
-		this.registerCommand('debugShowWorkspaceStorage');
-		this.registerCommand('debugClearAllWorkspaceStorage');
-
-		this.statusBarItem.command = this.makeCommandId('clickStatusBarItem');
-	}
-
 	private updateStatusBarItem = () => {
-		// TODO: Move all time-based code to StartStopTimes class and remove luxon import
+		// TODO: Move all time-based code to StartStopTimes class and remove luxon import from this file
 		let intervalsStoppedNow: Interval[];
 		if (!this.startStopTimes.lastStartTime) {
 			intervalsStoppedNow = this.startStopTimes.intervals;
@@ -85,76 +55,106 @@ export class StatusBarTimer implements StatusBarCommands {
 		this.statusBarItem.text = `$(${icon}) ${duration.toFormat('hh:mm:ss')}`;
 	};
 
-	startTimer = () => {
-		if (!this.startStopTimes.lastStartTime) {
-			this.startStopTimes.lastStartTime = DateTime.utc();
-		} else {
-			vscode.window.showInformationMessage('Timer already started');
-		}
-
-		this.startStopTimes.saveToStorage(this.context.workspaceState);
-		this.updateStatusBarItem();
-	};
-
-	stopTimer = () => {
-		if (!this.startStopTimes.lastStartTime) {
-			vscode.window.showInformationMessage('Timer already stopped');
-		} else {
-			this.startStopTimes.intervals.push(
-				Interval.fromDateTimes(this.startStopTimes.lastStartTime, DateTime.utc())
-			);
-			this.startStopTimes.lastStartTime = undefined;
-		}
-
-		this.startStopTimes.saveToStorage(this.context.workspaceState);
-		this.updateStatusBarItem();
-	};
-
-	resetTimer = () => {
-		this.startStopTimes.reset();
-		this.startStopTimes.saveToStorage(this.context.workspaceState);
-		this.updateStatusBarItem();
-	};
-
-	clickStatusBarItem = () => {
-		if (!this.startStopTimes.lastStartTime) {
-			this.startTimer();
-		} else {
-			this.stopTimer();
-		}
-	};
-
-	debugShowWorkspaceStorage = () => {
-		const data: { [key: string]: any } = {};
-		for (const key of this.context.workspaceState.keys()) {
-			data[key] = this.context.workspaceState.get(key);
-		}
-
-		if (data.length === 0) {
-			console.log('Workspace storage empty');
-		} else {
-			console.log(`Workspace storage:\n${JSON.stringify(data, null, 2)}`);
-		}
-	};
-
-	debugClearAllWorkspaceStorage = () => {
-		const no = 'No';
-		const yes = 'Yes';
-		vscode.window.showQuickPick(
-			[no, yes],
-			{ title: `Clear all workspace storage for ${this.getDisplayName()}?`}
-		).then((value) => {
-			if (value !== yes) {
-				return;
+	private readonly commands = {
+		startTimer: () => {
+			if (!this.startStopTimes.lastStartTime) {
+				this.startStopTimes.lastStartTime = DateTime.utc();
+			} else {
+				vscode.window.showInformationMessage('Timer already started');
 			}
 
+			this.startStopTimes.saveToStorage(this.context.workspaceState);
+			this.updateStatusBarItem();
+
+		},
+
+		stopTimer: () => {
+			if (!this.startStopTimes.lastStartTime) {
+				vscode.window.showInformationMessage('Timer already stopped');
+			} else {
+				this.startStopTimes.intervals.push(
+					Interval.fromDateTimes(this.startStopTimes.lastStartTime, DateTime.utc())
+				);
+				this.startStopTimes.lastStartTime = undefined;
+			}
+
+			this.startStopTimes.saveToStorage(this.context.workspaceState);
+			this.updateStatusBarItem();
+
+
+		},
+
+		resetTimer: () => {
+			this.startStopTimes.reset();
+			this.startStopTimes.saveToStorage(this.context.workspaceState);
+			this.updateStatusBarItem();
+
+
+		},
+
+		clickStatusBarItem: () => {
+			if (!this.startStopTimes.lastStartTime) {
+				this.commands.startTimer();
+			} else {
+				this.commands.stopTimer();
+			}
+		},
+
+		debugShowWorkspaceStorage: () => {
+			const data: { [key: string]: any } = {};
 			for (const key of this.context.workspaceState.keys()) {
-				this.context.workspaceState.update(key, undefined);
+				data[key] = this.context.workspaceState.get(key);
 			}
 
-			this.startStopTimes.loadFromStorage(this.context.workspaceState);
+			if (data.length === 0) {
+				console.log('Workspace storage empty');
+			} else {
+				console.log(`Workspace storage:\n${JSON.stringify(data, null, 2)}`);
+			}
+		},
 
-			vscode.window.showInformationMessage('Cleared workspace storage');
-		});
+		debugClearAllWorkspaceStorage: () => {
+			const no = 'No';
+			const yes = 'Yes';
+			vscode.window.showQuickPick(
+				[no, yes],
+				{ title: `Clear all workspace storage for ${this.getDisplayName()}?` }
+			).then((value) => {
+				if (value !== yes) {
+					return;
+				}
+
+				for (const key of this.context.workspaceState.keys()) {
+					this.context.workspaceState.update(key, undefined);
+				}
+
+				this.startStopTimes.loadFromStorage(this.context.workspaceState);
+
+				vscode.window.showInformationMessage('Cleared workspace storage');
+			});
+
+		},
+	} as const;
+
+	private makeCommandId = (commandName: keyof typeof this.commands) => {
+		return `${this.context.extension.packageJSON.name}.${commandName}`;
+	};
+
+	private registerCommand = (commandName: keyof typeof this.commands) => {
+		this.context.subscriptions.push(vscode.commands.registerCommand(
+			this.makeCommandId(commandName),
+			this.commands[commandName]
+		));
+	};
+
+	private registerCommands = () => {
+		this.registerCommand('startTimer');
+		this.registerCommand('stopTimer');
+		this.registerCommand('resetTimer');
+		this.registerCommand('clickStatusBarItem');
+		this.registerCommand('debugShowWorkspaceStorage');
+		this.registerCommand('debugClearAllWorkspaceStorage');
+
+		this.statusBarItem.command = this.makeCommandId('clickStatusBarItem');
 	};
 }
