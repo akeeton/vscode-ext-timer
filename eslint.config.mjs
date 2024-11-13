8dbfe39d56e1f6fe22b99e99d0abc94f205e014b
akeeton <andrewrkeeton@gmail.com>
2024-11-13 15:59:43 -0500

    Revert "Actually use Airbnb ESlint config (earlier commit didn't work)"
    This reverts commit 129f00b74149b18c5caa1f44f7c688ced52eab5a.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,28 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-// BEGIN: FlatCompat workaround for importing older, eslintrc-based plugins that
-// use "extends", namely eslint-config-airbnb-base ('airbnb-base' below)
-// See https://www.raulmelo.me/en/blog/migration-eslint-to-flat-config#fixing-compatibility-issues
-import { FlatCompat } from '@eslint/eslintrc';
-import path from 'path';
-import { fileURLToPath } from 'url';
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname, // optional; default: process.cwd()
-  resolvePluginsRelativeTo: __dirname, // optional
-});
-// END: FlatCompat workaround
-
-/** @type {import('eslint').Linter.FlatConfig[]} */
-export default [
-  {
-    files: ['**/*.ts'],
-  },
-  ...compat.extends('airbnb-base'), // See FlatCompat workaround above
-  {
+export default [{
+    files: ["**/*.ts"],
+}, {
     plugins: {
-      '@typescript-eslint': typescriptEslint,
+        "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: 'module',
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "module",
     },
 
     rules: {
-      '@typescript-eslint/naming-convention': ['warn', {
-        selector: 'import',
-        format: ['camelCase', 'PascalCase'],
-      }],
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "import",
+            format: ["camelCase", "PascalCase"],
+        }],
 
-      curly: 'warn',
-      eqeqeq: 'warn',
-      'no-throw-literal': 'warn',
-      semi: 'warn',
-
-      // BEGIN: Modify some rules set by Airbnb config
-      'import/extensions': ['error', {
-        ts: 'never',
-      }],
-      'import/no-unresolved': 'off',
-      // END: Modify some rules set by Airbnb config
+        curly: "warn",
+        eqeqeq: "warn",
+        "no-throw-literal": "warn",
+        semi: "warn",
     },
-  },
-];
+}];
