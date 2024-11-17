214acb230dd9a0d507df1924b3d999af5a95b259
akeeton <andrewrkeeton@gmail.com>
2024-11-17 11:14:30 -0500

    Add eslint-plugin-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,43 +2,47 @@
 // Adapted from `yo code` generator and then https://typescript-eslint.io/users/configs/
 
 import eslint from "@eslint/js";
-import stylisticTs from "@stylistic/eslint-plugin-ts";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 export default [
-    eslint.configs.recommended,
-    // ...tseslint.configs.recommendedTypeChecked,
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-    {
-        files: ["**/*.ts"],
-    }, {
-        plugins: {
-            "@stylistic/ts": stylisticTs,
-            "@typescript-eslint": tseslint.plugin,
+  eslint.configs.recommended,
+  // ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintPluginPrettierRecommended, // Also enables eslint-config-prettier
+  {
+    files: ["**/*.ts"],
+  },
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
         },
-        languageOptions: {
-            parser: tseslint.parser,
-            ecmaVersion: 2022,
-            sourceType: "module",
-            parserOptions: {
-                projectService: {
-                    allowDefaultProject: ['eslint.config.mjs'],
-                },
-                tsconfigRootDir: import.meta.dirname,
-            }
-        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
 
-        rules: {
-            "@stylistic/ts/semi": "warn",
-            "@typescript-eslint/naming-convention": ["warn", {
-                selector: "import",
-                format: ["camelCase", "PascalCase"],
-            }],
-
-            curly: "warn",
-            eqeqeq: "warn",
-            "no-throw-literal": "warn",
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"],
         },
-    }];
+      ],
+
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
+    },
+  },
+];
